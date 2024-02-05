fcedd0b04b3b472670fb53d5ff08e0b740b53bd4
Name <123@mail.ru>
2024-02-05 14:50:34 +0300

    С семинара

---
+++ data_create.py
@@ -0,0 +1,19 @@
+def name_data():
+    name = input('Введите имя: ')
+    print('Видел и похуже =)')
+    return name
+
+def surname_data():
+    surname = input('Введите фамилию: ')
+    print('А мне еще показалось, что имя неочень')
+    return surname
+
+def phone_data():
+    phone = input('Введите телефон: ')
+    print('И как это вы запомнили столько цифр?!!')
+    return phone
+
+def address_data():
+    address = input('Введите адрес: ')
+    print('Так бы и писал, что БОМЖ!')
+    return address
